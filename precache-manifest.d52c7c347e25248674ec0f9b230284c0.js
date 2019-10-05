7544937abf57d0c7cd181169a0b5706223d39ae6
Ayush Poddar <ayush_poddar@live.com>
2019-10-05 12:18:35 +0530

    Updates

---
+++ precache-manifest.d52c7c347e25248674ec0f9b230284c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9981b468a81c1bd9eeefee9d3b860623",
+    "url": "/react-page/index.html"
+  },
+  {
+    "revision": "5035d3d4f0ba36579797",
+    "url": "/react-page/static/css/main.ebbee091.chunk.css"
+  },
+  {
+    "revision": "57cb029020b4b016a10c",
+    "url": "/react-page/static/js/2.3a4aef12.chunk.js"
+  },
+  {
+    "revision": "5035d3d4f0ba36579797",
+    "url": "/react-page/static/js/main.cf84e48f.chunk.js"
+  },
+  {
+    "revision": "1c4958ae21e6043b8094",
+    "url": "/react-page/static/js/runtime-main.a27b5407.js"
+  },
+  {
+    "revision": "b21aa7705d8ad6c7b467b095c6d3754b",
+    "url": "/react-page/static/media/card.b21aa770.jpg"
+  }
+]);
